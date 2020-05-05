1f57e17fb711917b5325b86d628084fa78812ac9
Laurent Tram <antaresscp@gmail.com>
2020-05-05 17:51:39 +0200

    Another try with dotenv

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
+require("dotenv").config();
 module.exports = {
   env: {
-    graphqlUrl: "http://localhost:3000/api/graphql",
+    // Reference a variable that was defined in the .env file and make it available at Build Time
+    GRAPHQL_URL: process.env.GRAPHQL_URL,
   },
 };
--- src/services/withApollo.js
+++ src/services/withApollo.js
@@ -1,11 +1,9 @@
 import withApollo from "next-with-apollo";
 import ApolloClient, { InMemoryCache } from "apollo-boost";
 
-const link = "https://hacker-news.firebaseio.com/v0/topstories";
-
 export default withApollo(({ initialState }) => {
   return new ApolloClient({
-    uri: process.env.graphqlUrl,
+    uri: process.env.GRAPHQL_URL,
     cache: new InMemoryCache().restore(initialState || {}),
   });
 });
